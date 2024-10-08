7bac366e1733c07fd59974aa864da9a3e260629f
Mikkel Tiller <mikkeltiller@gmail.com>
2024-10-08 11:13:19 +0200

    Add keybinds

--- init.lua
+++ init.lua
@@ -190,6 +190,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Escape with jk
+vim.keymap.set('i', 'jk', '<Esc>')
+
+-- Enter command mode with ø
+vim.keymap.set('n', 'ø', ':')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
