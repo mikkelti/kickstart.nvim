f3bff44a511b6e84051fdbcb07c421dd7a3d6e4f
Mikkel Tiller <mikkeltiller@gmail.com>
2026-09-18 13:32:40 +0200

    Highlight ros launch files as lua

--- init.lua
+++ init.lua
@@ -106,6 +106,8 @@ do
   -- NOTE: You can change these options as you wish!
   --  For more options, you can see `:help option-list`
 
+  vim.filetype.add { extension = { launch = 'xml' } }
+
   -- Make line numbers default
   vim.o.number = true
   -- You can also add relative line numbers, to help with jumping.
